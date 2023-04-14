592e8b21ad62a675c88dbafd4a793d49f82fe83c
agatticelli <agatticelli@gmail.com>
2023-04-14 03:09:28 -0300

    Add Client struct

--- form3/form3.go
+++ form3/form3.go
@@ -1,1 +1,29 @@
 package form3
+
+import (
+	"net/http"
+	"net/url"
+)
+
+const (
+	// defaultBaseURL is the default base URL for the Form3 API.
+	defaultBaseURL = "https://api.form3.tech/v1"
+)
+
+type Client struct {
+	// HTTP client used to make requests.
+	client *http.Client
+
+	// Base URL of the Form3 API.
+	BaseURL *url.URL
+}
+
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	baseURL, _ := url.Parse(defaultBaseURL)
+
+	return &Client{client: httpClient, BaseURL: baseURL}
+}
