0378c95649e45bbf48c35741bab9a7eb8ad0080f
agatticelli <agatticelli@gmail.com>
2023-04-14 03:23:52 -0300

    Add methods for preparing and handling http requests

--- form3/form3.go
+++ form3/form3.go
@@ -1,7 +1,11 @@
 package form3
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -56,4 +60,80 @@ func NewClient(httpClient *http.Client) *Client {
 	baseURL, _ := url.Parse(defaultBaseURL)
 
 	return &Client{client: httpClient, BaseURL: baseURL}
+}
+
+func (c *Client) Do(ctx context.Context, method, url string, body, result interface{}) error {
+	req, err := c.newRequest(ctx, method, url, body)
+	if err != nil {
+		return err
+	}
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	return c.decodeBody(res, result)
+}
+
+func (c *Client) newRequest(ctx context.Context, method, uri string, body interface{}) (*http.Request, error) {
+	parsedUri, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+	u := c.BaseURL.ResolveReference(parsedUri)
+
+	var marshalledBody []byte
+
+	if body != nil {
+		marshalledBody, err = json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, u.String(), bytes.NewReader(marshalledBody))
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	return req, nil
+}
+
+func (c *Client) decodeBody(res *http.Response, result interface{}) error {
+	if res.StatusCode == http.StatusNoContent {
+		return nil
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		if len(resBody) == 0 {
+			return &Form3APIError{
+				StatusCode: res.StatusCode,
+				Message:    http.StatusText(res.StatusCode),
+			}
+		}
+
+		var errorResult Form3BodyResponseError
+		err = json.Unmarshal(resBody, &errorResult)
+		if err != nil {
+			return err
+		}
+
+		return &Form3APIError{
+			StatusCode: res.StatusCode,
+			Message:    errorResult.ErrorMessage,
+		}
+	}
+
+	return json.Unmarshal(resBody, result)
 }
