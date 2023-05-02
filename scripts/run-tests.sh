dcb131f98f88d2523c8515658665922e2bf4f17c
agatticelli <agatticelli@gmail.com>
2023-05-02 08:44:18 -0300

    Make sure exit status is not 0

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,15 @@
 
 # Run integration tests
 go test -v ./test/...
+if [ $? -ne 0 ]; then
+    echo "Integration tests failed"
+    exit 1
+fi
 
 # Run unit tests
 cd form3
 go test -v ./...
+if [ $? -ne 0 ]; then
+    echo "Unit tests failed"
+    exit 1
+fi
